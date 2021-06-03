499906e5907b12be94ea30a8c22c68cae9f80817
arogya bichpuria <arogyabichpuria16@gmail.com>
2021-06-03 16:04:56 +0530

    first prototype of counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Counter from "./logger";
+import CounterHooks from "./CounterHooks";
+
+
+export const ThemeContext = React.createContext()
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const [theme, setTheme] = useState('red')
+  return (
+
+    <ThemeContext.Provider value={{ backgroundColor: theme }} >
+      CounterHooks
+      <CounterHooks initialCount={0} />
+      Counter
+      <Counter initialCount={0} />   
+      <button onClick={()=>setTheme(prevTheme=>{return prevTheme==='red' ? 'blue': 'red' })}>Toggle theme</button>
+    </ThemeContext.Provider>
+  )
+  }
+
+  export default App;
